383ec6aea58b4110e0c531ba4af7ef5dcabda243
IZIK <84680298+izikdepth@users.noreply.github.com>
2023-10-29 12:53:27 +0100

    Update help.py

--- Qubic/Cogs/help.py
+++ Qubic/Cogs/help.py
@@ -8,6 +8,9 @@ class Help(commands.Cog):
 
     @commands.slash_command(description="help command")
     async def help(self,ctx):
+
+        initial_response = await ctx.send(content="Processing your request...")
+
         message = (
             "/ask - you can use this command to see how much USD you'll get for selling Qubic. Simple type ask and enter the qubic quantity\n"
             "/bid - you can use this command to see how much qubic you'll get for your USD. simply type /bid and enter the usd you want to buy with.\n"
@@ -15,7 +18,7 @@ class Help(commands.Cog):
             "/help to view all commands"
         )
         
-        await ctx.send(message)
+        await initial_response.edit(content=message)
         
 def setup(bot):
     bot.add_cog(Help(bot))
